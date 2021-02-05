dca10e55955d6dd3e00092d003bcd764c49ab4ae
Norman Hooper <nhooper@dimagi.com>
2021-02-05 16:42:17 +0200

    Tests

---
+++ tests/test_exceptions.py
@@ -0,0 +1,20 @@
+import pytest
+
+from jsonpath_ng import parse as rw_parse
+from jsonpath_ng.exceptions import JSONPathError, JsonPathParserError
+from jsonpath_ng.ext import parse as ext_parse
+
+
+def test_rw_exception_class():
+    with pytest.raises(JSONPathError):
+        rw_parse('foo.bar.`grandparent`.baz')
+
+
+def test_rw_exception_subclass():
+    with pytest.raises(JsonPathParserError):
+        rw_parse('foo.bar.`grandparent`.baz')
+
+
+def test_ext_exception_subclass():
+    with pytest.raises(JsonPathParserError):
+        ext_parse('foo.bar.`grandparent`.baz')
